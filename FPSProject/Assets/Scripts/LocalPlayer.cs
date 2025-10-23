4e67eb733d2812ccbbf3bce65ba86d2b2b4c948f
SUBcharacter <farg557@gmail.com>
2025-10-24 01:23:25 +0900

    플레이어 이동 구현중

--- FPSProject/Assets/Scripts/LocalPlayer.cs
+++ FPSProject/Assets/Scripts/LocalPlayer.cs
@@ -4,15 +4,21 @@ using UnityEngine.InputSystem;
 public class LocalPlayer : MonoBehaviour
 {
     [SerializeField] Rigidbody rigid;
-    [SerializeField] Transform horizontal;
+    [SerializeField] Transform vertical;
     [SerializeField] Animator[] animators;
 
-    [SerializeField] Vector2 horizonFovVector;
-    [SerializeField] Vector2 verticalFovVector;
+    [SerializeField] Vector2 mouseInputVec;
+    [SerializeField] Vector2 moveInputVec;
+
+    [SerializeField] float verticalRotation = 0;
+    [SerializeField] float mouseSensitivity;
+    [SerializeField] float maxMouseSensitivity;
 
     private void Awake()
     {
+        rigid = GetComponent<Rigidbody>();
         animators = GetComponentsInChildren<Animator>();
+        Cursor.lockState = CursorLockMode.Locked;
     }
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
@@ -27,10 +33,38 @@ public class LocalPlayer : MonoBehaviour
         
     }
 
+    void View()
+    {
+        float mouseX = mouseInputVec.x * mouseSensitivity;
+        float mouseY = -mouseInputVec.y * mouseSensitivity;
+
+        verticalRotation += mouseY;
+        verticalRotation = Mathf.Clamp(verticalRotation, -85f, 85f);
+
+        vertical.localRotation = Quaternion.Euler(verticalRotation, 0f, 0f);
+        transform.Rotate(Vector3.up, mouseX);
+    }
+
+    void Move()
+    {
+
+    }
+
     public void OnLook(InputAction.CallbackContext context)
     {
-        if (!context.performed)
-            return;
+        mouseInputVec = context.ReadValue<Vector2>();
+        View();
+    }
 
+    public void OnMove(InputAction.CallbackContext context)
+    {
+        if(context.performed)
+        {
+            moveInputVec = context.ReadValue<Vector2>();
+        }
+        else if(context.canceled)
+        {
+            moveInputVec = new Vector2(0, 0);
+        }
     }
 }
